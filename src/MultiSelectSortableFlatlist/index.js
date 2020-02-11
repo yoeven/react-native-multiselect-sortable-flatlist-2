b1907099ffee584af2d345c8dbc368fd1a4f14c5
Yoeven D Khemlani <yoevenkhem@gmail.com>
2020-02-11 16:41:47 +0800

    Update index.js

--- src/MultiSelectSortableFlatlist/index.js
+++ src/MultiSelectSortableFlatlist/index.js
@@ -11,12 +11,14 @@ export default class MultiSelectSortableFlatlist extends React.Component {
     onSort: () => {},
     sortable: true,
     selectable: true,
+    mode: 'auto',
+    initialSelectedItems: [],
   };
 
   constructor(props) {
     super(props);
     this.state = {
-      selectedItems: [],
+      selectedItems: [...this.props.initialSelectedItems],
     };
   }
 
@@ -51,9 +53,12 @@ export default class MultiSelectSortableFlatlist extends React.Component {
   }
 
   SelectAll() {
-    if (!this.props.selectable) return;
     this.setState({ selectedItems: [...this.props.data] });
     return [...this.props.data];
+  }
+
+  SelectionOverwrite(SelectedItems) {
+    this.setState({ selectedItems: [...SelectedItems] });
   }
 
   GetIndexofItem(item) {
@@ -80,28 +85,58 @@ export default class MultiSelectSortableFlatlist extends React.Component {
     this.props.onSort(newDataOrder);
   }
 
+  ReverseSelection(item, index) {
+    const indexOfIndex = this.GetIndexofItem(item);
+    const selectedItems = this.state.selectedItems;
+    if (indexOfIndex > -1) {
+      selectedItems.splice(indexOfIndex, 1);
+      this.setState({ selectedItems: selectedItems });
+      this.props.onItemDeselected({ selectedItems, item, index });
+    } else {
+      this.AddSelectedIndex(item, index);
+    }
+  }
+
+  renderItem(item, index, drag, isActive) {
+    var Component;
+    const Mode = this.props.mode;
+    if (Mode == 'auto') {
+      Component = (
+        <TouchableWithoutFeedback
+          onPress={() => this.OnTap(item, index)}
+          onLongPress={() => {
+            this.AddSelectedIndex(item, index, drag);
+          }}>
+          <View>
+            {this.props.renderItem({
+              item,
+              index,
+              selected: this.IsItemSelected(item),
+              isDragging: isActive,
+            })}
+          </View>
+        </TouchableWithoutFeedback>
+      );
+    } else if (Mode == 'manual') {
+      Component = this.props.renderItem({
+        item,
+        index,
+        selected: this.IsItemSelected(item),
+        drag: drag,
+        isDragging: isActive,
+        reverseSelection: () => this.ReverseSelection(item, index),
+      });
+    }
+    return Component;
+  }
+
   render() {
     return (
       <DraggableFlatList
         {...this.props}
         onDragEnd={({ data }) => this.OnSort(data)}
-        extraData={[this.state, { ...this.props.extraData }]}
-        renderItem={({ item, index, drag, isActive }) => (
-          <TouchableWithoutFeedback
-            onPress={() => this.OnTap(item, index)}
-            onLongPress={() => {
-              this.AddSelectedIndex(item, index, drag);
-            }}>
-            <View>
-              {this.props.renderItem({
-                item,
-                index,
-                selected: this.IsItemSelected(item),
-                isDragging: isActive,
-              })}
-            </View>
-          </TouchableWithoutFeedback>
-        )}
+        extraData={[this.state.selectedItems, { ...this.props.extraData }]}
+        renderItem={({ item, index, drag, isActive }) => this.renderItem(item, index, drag, isActive)}
       />
     );
   }
